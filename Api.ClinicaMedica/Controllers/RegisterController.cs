14446f866bc82af664fc8dc593c65c230495bddb
euncos <euncos@gmail.com>
2025-02-21 11:46:20 -0300

    Agregado tabla usuarios, registro usuario, token

--- Api.ClinicaMedica/Controllers/RegisterController.cs
+++ Api.ClinicaMedica/Controllers/RegisterController.cs
@@ -28,7 +28,7 @@ namespace Api.ClinicaMedica.Controllers
             _confi = confi;
         }
 
-        [HttpPost("register-medico")]
+        [HttpPost("register-paciente")]
         public async Task<ActionResult> RegisterPaciente([FromBody] PacientesCreateDTO pacientesCreateDTO)
         {
             if (!ModelState.IsValid)
@@ -62,15 +62,20 @@ namespace Api.ClinicaMedica.Controllers
                     IdPaciente = pacientesCreateDTO.IdPaciente,
                     Nombre = pacientesCreateDTO.Nombre,
                     Apellido = pacientesCreateDTO.Apellido,
+                    Dni = pacientesCreateDTO.Dni,
                     Email = pacientesCreateDTO.Email,
+                    FechaNacimiento = pacientesCreateDTO.FechaNacimiento,
+                    Telefono = pacientesCreateDTO.Telefono,
+                    Direccion = pacientesCreateDTO.Direccion,
                     Password = hashedPassword,
-                    IdRol = pacientesCreateDTO.IdRol
+                    IdRol = pacientesCreateDTO.IdRol,
+                    ObraSocial = pacientesCreateDTO.ObraSocial
                 };
 
                 _context.Pacientes.Add(paciente);
                 await _context.SaveChangesAsync();
 
-                return Ok("Usuario registrado con éxito.");
+                return Ok("Paciente registrado con éxito.");
 
             }
             catch (DbUpdateException ex)
@@ -85,7 +90,7 @@ namespace Api.ClinicaMedica.Controllers
             }
         }
 
-        [HttpPost("register-paciente")]
+        [HttpPost("register-medico")]
         public async Task<ActionResult> RegisterMedico([FromBody] MedicosCreateDTO medicosCreateDTO)
         {
             if (!ModelState.IsValid)
@@ -117,15 +122,81 @@ namespace Api.ClinicaMedica.Controllers
                 var medico = new Medicos
                 {
                     IdMedico = medicosCreateDTO.IdMedico,
-                    IdEspecialidad = medicosCreateDTO.IdEspecialidad,
                     Nombre = medicosCreateDTO.Nombre,
                     Apellido = medicosCreateDTO.Apellido,
+                    Dni = medicosCreateDTO.Dni,
                     Email = medicosCreateDTO.Email,
+                    FechaNacimiento = medicosCreateDTO.FechaNacimiento,
+                    Telefono = medicosCreateDTO.Telefono,
+                    Direccion = medicosCreateDTO.Direccion,
+                    IdRol = medicosCreateDTO.IdRol,
+                    IdEspecialidad = medicosCreateDTO.IdEspecialidad,
                     Password = hashedPassword,
-                    IdRol = medicosCreateDTO.IdRol
+                    Sueldo = medicosCreateDTO.Sueldo
                 };
 
                 _context.Medicos.Add(medico);
+                await _context.SaveChangesAsync();
+
+                return Ok("Medico registrado con éxito.");
+
+            }
+            catch (DbUpdateException ex)
+            {
+                var innerException = ex.InnerException?.Message;
+                return StatusCode(500, $"Error al guardar en la base de datos: {innerException}");
+            }
+
+            catch (Exception ex)
+            {
+                return StatusCode(500, "Ocurrió un error inesperado. " + ex.Message);
+            }
+        }
+
+        [HttpPost("register-usuario")]
+        public async Task<ActionResult> RegisterUsuario([FromBody] UsuariosCreateDTO usuariosCreateDTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+
+            }
+            try
+            {
+                //Validar si el correo ya esta registrado
+                bool emailExists = await _context.Usuarios.AnyAsync(u => u.Email == usuariosCreateDTO.Email);
+                if (emailExists)
+                {
+                    return BadRequest("El correo ya está registrado.");
+                }
+
+                //validar si el rol existe en la DB
+                var rol = await _context.Roles.FindAsync(usuariosCreateDTO.IdRol);
+                if (rol == null)
+                {
+                    return BadRequest("El rol especificado no existe.");
+                }
+
+                //Encriptacion de la contraseña
+
+                string hashedPassword = BCrypt.Net.BCrypt.HashPassword(usuariosCreateDTO.Password);
+
+                // Crear un nuevo usuario
+                var usuario = new Usuarios
+                {
+                    IdUsuario = usuariosCreateDTO.IdUsuario,
+                    Nombre = usuariosCreateDTO.Nombre,
+                    Apellido = usuariosCreateDTO.Apellido,
+                    Dni = usuariosCreateDTO.Dni,
+                    Email = usuariosCreateDTO.Email,
+                    FechaNacimiento = usuariosCreateDTO.FechaNacimiento,
+                    Telefono = usuariosCreateDTO.Telefono,
+                    Direccion = usuariosCreateDTO.Direccion,
+                    IdRol = usuariosCreateDTO.IdRol,
+                    Password = hashedPassword,
+                };
+
+                _context.Usuarios.Add(usuario);
                 await _context.SaveChangesAsync();
 
                 return Ok("Usuario registrado con éxito.");
@@ -150,6 +221,14 @@ namespace Api.ClinicaMedica.Controllers
             var listaPersonas = await _context.Pacientes.ToListAsync();
             return _mapper.Map<List<PacientesDTO>>(listaPersonas);
         }
+
+        [HttpGet("obtener-medicos")]
+        public async Task<ActionResult<IEnumerable<MedicosDTO>>> GetMedicos()
+        {
+            var listaMedicos = await _context.Pacientes.ToListAsync();
+            return _mapper.Map<List<MedicosDTO>>(listaMedicos);
+        }
+
 
         [HttpPost("Login")]
         public async Task<IActionResult> Login([FromBody] LoginDTO loginDTO)
---
+++ Api.ClinicaMedica/DTO/Create/UsuariosCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Api.ClinicaMedica.DTO.Create
+{
+    public class UsuariosCreateDTO
+    {
+        public string IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string? Dni { get; set; }
+        public string? Email { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string? Telefono { get; set; }
+        public string? Direccion { get; set; }
+        public string Password { get; set; } = null!;
+        public int IdRol { get; set; }
+    }
+}
